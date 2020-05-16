02280bd13628c940ae749c1f527ac49da2b53362
Maxim Zhiburt <zhiburt@gmail.com>
2020-05-16 16:35:54 +0300

    flags: update documentation
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

--- src/args.rs
+++ src/args.rs
@@ -1,22 +1,24 @@
 use clap::Clap;
 
 #[derive(Clap)]
-#[clap(version = "0.1", author = "Maxim Z.")]
+#[clap(
+    author = "Maxim Z. <zhiburt@gmail.com>",
+    about = "
+Niping is an implementation of ping in rust. 
+It mimics the interface of original ping."
+)]
 pub struct Opts {
     /// Setting of the IP Time to Live.
     #[clap(short = "t")]
     pub ttl: Option<u32>,
-    /// Time to wait for a response, in seconds..
-    ///
-    /// We are not handle the timeout properly yet.
-    /// Thus currently you can only recognize the type of error by that.
-    #[clap(short = "W")]
+    /// Time to wait for a response, in seconds.
+    #[clap(short = "W", name="timeout")]
     pub read_timeout: Option<u32>,
     /// Stop after sending count ECHO_REQUEST packets.
-    #[clap(short = "c")]
+    #[clap(short = "c", name="count")]
     pub count_packets: Option<usize>,
-    /// Wait interval seconds between sending each packet.
-    #[clap(short = "i")]
+    /// Wait interval seconds between sending each packet. The default value is 1 second.
+    #[clap(short = "i", name="interval")]
     pub send_interval: Option<f32>,
     /// The address ping which
     pub address: String,
